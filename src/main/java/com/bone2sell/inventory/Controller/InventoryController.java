5ca9923f2150168298b85608b165947957d091f2
Abhimanyu <abhimanyusinghanand02@gmail.com>
2025-04-12 12:36:01 +0530

    This is my fourth commit of the day.

--- src/main/java/com/bone2sell/inventory/Controller/InventoryController.java
+++ src/main/java/com/bone2sell/inventory/Controller/InventoryController.java
@@ -1,4 +1,10 @@
 package com.bone2sell.inventory.Controller;
+import com.bone2sell.inventory.Models.ClothItem;
+import com.bone2sell.inventory.Models.FoodItem;
+import com.bone2sell.inventory.Repository.ClothItemRepository;
+import com.bone2sell.inventory.Repository.FoodItemRepository;
+import com.bone2sell.inventory.Service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -6,13 +12,23 @@ import java.util.List;
 @RestController
 public class InventoryController {
 
+    private InventoryService iser;
+    @Autowired
+    public void InventoryController(InventoryService iser){
+        this.iser = iser;
+    }
+
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello user";
     }
 
     @PostMapping
-    public List<String> addFoodTtem(@RequestBody FoodItem food_item) {
-        return food_item.
+    public String addFoodTtem(@RequestBody FoodItem food) {
+        return iser.addFoodItem(food);
+    }
+    @PostMapping
+    public String addCloth(@RequestBody ClothItem food) {
+        return iser.addClothItem();
     }
 }
